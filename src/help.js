02946218786581ce63df2a5758a9276b5e2d3179
zuckzin <77843721+zuckzin@users.noreply.github.com>
2021-01-22 12:18:16 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,134 @@
+const help = (prefix) => { 
+	return `   *𝙕𝙐𝘾𝙆𝙕𝙄𝙉 𝘽𝙊𝙏 𝙏𝙀𝙎𝙏𝙀*
+	
+
+┃ *⚠️𝙉 𝙎𝙊𝙐 𝘾𝙍𝙄𝘼𝘿𝙊𝙍 𝘿𝙊 𝘽𝙊𝙏⚠️*
+┃   *𝕋𝕖𝕣𝕞𝕦𝕩 𝕊𝕔𝕙𝕠𝕠𝕝 𝔹𝕆𝕋 𝔹𝕣𝕒𝕤𝕚𝕝*
+┃
+┣• *${prefix}speed* ✓ velocidade ✓
+┃͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏ ͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏͏
+┣• *${prefix}stiker* ✓ converta imagem ou gif em figurinha ✓
+┣• *${prefix}tsticker*
+┣• *${prefix}toimg* ✓ converta figurinhas em imagens✓
+┣• *${prefix}nulis*
+┣• *${prefix}ocr*
+┣• *${prefix}ytsearch*
+┣• *${prefix}ytmp3*
+┣• *${prefix}ytmp4*
+┣• *${prefix}tiktok*
+┣• *${prefix}tiktokstalk*
+┣• *${prefix}fototiktok*
+┣⊱➤ *${prefix}igstalk*
+┣⊱➤ *${prefix}image*
+┣⊱➤ *${prefix}pinterest*
+┣⊱➤ *${prefix}tts*
+┣⊱➤ *${prefix}tes*
+┣⊱➤ *${prefix}tep*
+┣⊱➤ *${prefix}ttp*
+┣⊱➤ *${prefix}meme*
+┣⊱➤ *${prefix}memeindo*
+┣⊱➤ *${prefix}ssweb*
+┣⊱➤ *${prefix}walpaperhd*
+┣⊱➤ *${prefix}randomcat*
+┣⊱➤ *${prefix}joox*
+┣⊱➤ *${prefix}inu*
+┣⊱➤ *${prefix}elang*
+┣⊱➤ *${prefix}unta*
+┣⊱➤ *${prefix}anjing*
+┣⊱➤ *${prefix}babi*
+┣⊱➤ *${prefix}playstore*
+┣⊱➤ *${prefix}url2image*
+┣⊱➤ *${prefix}kbbi*
+┣⊱➤ *${prefix}imoji*
+┣⊱➤ *${prefix}wait*
+┣⊱➤ *${prefix}thunder*
+┣⊱➤ *${prefix}tahta*
+┣⊱➤ *${prefix}glitch <teks|teks>*
+┣⊱➤ *${prefix}phlogo <teks|teks>*
+┣⊱➤ *${prefix}wolflogo <teks|teks>*
+┣⊱➤ *${prefix}wolflogo2 <teks|teks>*
+┣⊱➤ *${prefix}quotemaker <tx|wtrmk|tema>*
+┣⊱➤ *${prefix}galaxtext*
+┣⊱➤ *${prefix}textdark*
+┣⊱➤ *${prefix}textblue*
+┣⊱➤ *${prefix}lovemake*
+┣⊱➤ *${prefix}stiltext*
+┣⊱➤ *${prefix}ninjalogo*
+┣⊱➤ *${prefix}party*
+┣⊱➤ *${prefix}rtext*
+┣⊱➤ *${prefix}water*
+┣⊱➤ *${prefix}lionlogo <teks|teks>*
+┣⊱➤ *${prefix}textscreen*
+┣⊱➤ *${prefix}text3d*
+┣⊱➤ *${prefix}epep*
+┣⊱➤ *${prefix}marvelogo <teks|teks>*
+┣⊱➤ *${prefix}snow <teks|teks>*
+┣⊱➤ *${prefix}firetext*
+┣⊱➤ *${prefix}openanime*  ✘
+┣⊱➤ *${prefix}Minato*
+┣⊱➤ *${prefix}Boruto*
+┣⊱➤ *${prefix}Hinata*
+┣⊱➤ *${prefix}Sakura*
+┣⊱➤ *${prefix}Sasuke*
+┣⊱➤ *${prefix}kaneki*
+┣⊱➤ *${prefix}toukacan*
+┣⊱➤ *${prefix}rize*
+┣⊱➤ *${prefix}akira*
+┣⊱➤ *${prefix}itori*
+┣⊱➤ *${prefix}kurumi*
+┣⊱➤ *${prefix}miku*
+┣⊱➤ *${prefix}nekonime*
+┣⊱➤ *${prefix}waifu*
+┣⊱➤ *${prefix}waifu2*
+┣⊱➤ *${prefix}wibu*
+┣⊱➤ *${prefix}randomanime*
+┣⊱➤ *${prefix}pokemon*
+┣⊱➤ *${prefix}artinama*
+┣⊱➤ *${prefix}add*
+┣⊱➤ *${prefix}kick* ✓ remover um membro /kick e marque o membro
+┣⊱➤ *${prefix}promote* /promote ✓ promover um membro adm marque o membro✓
+┣⊱➤ *${prefix}demote*
+┣⊱➤ *${prefix}setname* /setname ✓ alterar nome do grupo✓
+┣⊱➤ *${prefix}setdesc* /setdesc ✓ apagar descric do grupo✓
+┣⊱➤ *${prefix}welcome*
+┣⊱➤ *${prefix}nsfw*
+┣⊱➤ *${prefix}simih*
+┣⊱➤ *${prefix}grup [buka/tutup]* ✓ grup buka (abre o grupo) grup tutup fecha o grupo✓
+┣⊱➤ *${prefix}tagme*
+┣⊱➤ *${prefix}hidetag*
+┣⊱➤ *${prefix}tagall* /tagall ✓marcar todos membros do grupo inclusive adms✓
+┣⊱➤ *${prefix}otagall*
+┣⊱➤ *${prefix}fitnah*
+┣⊱➤ *${prefix}infogc* ✓numero de adms e membros✓
+┣⊱➤ *${prefix}grupinfo*
+┣⊱➤ *${prefix}linkgrup*
+┣⊱➤ *${prefix}listadmins*
+┣⊱➤ *${prefix}openanime*
+┣⊱➤ *${prefix}edotense*
+┣⊱➤ *${prefix}kudeta*
+┣⊱➤ *${prefix}nsfwblowjob*
+┣⊱➤ *${prefix}nsfwneko*
+┣⊱➤ *${prefix}nsfwtrap*
+┣⊱➤ *${prefix}randomhentai*
+┣⊱➤ *${prefix}hentai*
+┣⊱➤ *${prefix}indohot*
+┣⊱➤ *${prefix}blocklist*contatos bloqueados pelo bot✓
+┣⊱➤ *${prefix}testime* ✓hora do teste✓
+┣⊱➤ *${prefix}hilih*
+┣⊱➤ *${prefix}say*
+┣⊱➤ *${prefix}delete*
+┣⊱➤ *${prefix}shorturl*
+┣⊱➤ *${prefix}bc*
+┣⊱➤ *${prefix}ban*
+┣⊱➤ *${prefix}block*
+┣⊱➤ *${prefix}unblock*
+┣⊱➤ *${prefix}clearall*
+┣⊱➤ *${prefix}clone* ✓clonar foto de alguém para usar no perfil do bot marque a pessoa✓
+┣⊱➤ *${prefix}getses*
+┣⊱➤ *${prefix}setpp*  ✓envie a foto com a legenda para mudar o icone do grupo✓
+┣⊱➤ *${prefix}setpp*
+┣━━━━━━━━━━━━━━━━━━━━┓
+┃        *❬𝙕𝙐𝘾𝙆𝙕𝙄𝙉❭*
+┗━━━━━━━━━━━━━━━━━━━━┛`
+}
+exports.help = help
